401cb2ede74c926fc8d41ad212252833ff7b0a4a
dazycokar <dayocoker15@gmail.com>
2025-01-28 12:57:56 -0800

    commiting header code

---
+++ src/components/Header.js
@@ -0,0 +1,34 @@
+import "./Header.css";
+
+function Header() {
+  return (
+    <div className="body">
+      <div class="translator">
+        <h1>Free AI Translator Powered by ChatGPT 3.5/4</h1>
+      </div>
+      <div className="description">
+        <h2>Description</h2>
+        <br></br>
+        <p>
+          image.ai translator help to converts written or spoken content from
+          one language to another while preserving its original meaning, tone
+          and intent.
+        </p>
+        <p>
+          image.ai translator work in various fields, such as literature, legal
+          documents, technical manuals, business communication, and more.
+        </p>
+        <p>
+          image.ai often uses specialized tools like glossaries, CAT
+          (computer-assisted translation) software, or AI tools to enchance its
+          efficiency and accuracy.
+        </p>
+      </div>
+      <div>
+        <button class="text">Get Started</button>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
