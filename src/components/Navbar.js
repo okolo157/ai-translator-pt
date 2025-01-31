7242265387443df3638261c87910bddf95bc2fd3
Anointing-olabode <anointingolabode@gmail.com>
2025-01-31 10:13:58 +0100

    new commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import '../styles/Navbar.css'
+
+export default function Navbar(){
+    return <div className="New">
+        {/* <img src="c:\Users\major\Downloads\icons8-home-50.png"></img> */}
+        <div className="New2">
+        <a>Features</a>
+        <a>Tutorial</a>
+        <a>Pricing</a>
+        <a>Login</a>
+        </div>
+    </div>
+}
