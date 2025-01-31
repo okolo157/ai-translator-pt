b555aa7a96b0ea10ae14b492a6d5934cb7842367
Timi379 <holluwatimilehin379@gmail.com>
2025-01-31 13:40:22 +0100

    Update Rating.js

--- src/components/translator/Rating.js
+++ src/components/translator/Rating.js
@@ -1,8 +1,42 @@
 import React from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+// import { FontAwesomeIcon } from "@fortawesome/fontawesome-svg-core"
+import { faHistory, faGift, faStar } from "@fortawesome/free-solid-svg-icons"
+import "./Rating.css"
+// import { faStar } from '../../../node_modules/@fortawesome/free-regular-svg-icons';
+
+
+
 
 function Rating() {
   return (
-    <div>Rating</div>
+    <div className="new">
+      <div className="container">
+        <div className="Star">
+          Rate This tool  <FontAwesomeIcon icon={faStar} color="Yellow" />
+          <FontAwesomeIcon icon={faStar} color="Yellow" />
+          <FontAwesomeIcon icon={faStar} color="Yellow" />
+          <FontAwesomeIcon icon={faStar} color="Yellow" />
+          <FontAwesomeIcon icon={faStar} color="Yellow" />
+        </div>
+
+        <div className="Number">
+          4.9/5
+      </div>
+        <div className="His">
+          <FontAwesomeIcon icon={faHistory} color="blue" /> History
+      </div>
+      </div>
+
+      <div className="Sec">
+
+
+        <div className="Get">
+          <FontAwesomeIcon icon={faGift} color="yellow" /> Get Free GPT -4 Words
+      </div>
+      </div>
+
+    </div>
   )
 }
 
