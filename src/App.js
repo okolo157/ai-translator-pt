aacaf7a9ad2485d40b326d663e88ffc8166101cc
vik1234-del <victor.okolo@pau.edu.ng>
2025-02-06 09:43:42 +0100

    lates changes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import "./App.css";
 // import TranslateComponent from "./components/TranslateComponent";
 import Landing from "./pages/Landing";
 import Signup from "./pages/Signup";
+import Signin from "./pages/Signin";
+import Pricing from "./pages/Pricing";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
@@ -10,6 +12,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/Signin" element={<Signin />} />
+        <Route path="/pricing" element={<Pricing />} />
       </Routes>
     </div>
   );
