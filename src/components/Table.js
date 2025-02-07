abf3a7c3efc4e2b7ada6d1335ca7171543e260e0
dazycokar <dayocoker15@gmail.com>
2025-02-07 10:04:16 -0800

    adding pricing page elements

---
+++ src/components/Table.js
@@ -0,0 +1,50 @@
+import "../styles/Table.css";
+
+function Table() {
+  return (
+    <div className="table">
+      <div className="component">
+        <h1>Compare Plans</h1>
+        <div className="table2">
+          <div className="features">
+            <h2>Features</h2>
+            <p>Free</p>
+            <p>Pro</p>
+            <p>Enterprise</p>
+          </div>{" "}
+          <hr />
+          <div className="features">
+            <h2>Credit</h2>
+            <p>10/month</p>
+            <p>Unlimited</p>
+            <p>Unlimited</p>
+          </div>{" "}
+          <hr />
+          <div className="features">
+            <h2>Support</h2>
+            <p>Basic</p>
+            <p>Priority</p>
+            <p>Dedicated</p>
+          </div>{" "}
+          <hr />
+          <div className="features">
+            <h2>Custom Solutions</h2>
+            <p>No</p>
+            <p>No</p>
+            <p>Yes</p>
+          </div>{" "}
+          <hr />
+          <div className="features">
+            <h2>Access to All Features</h2>
+            <p>No</p>
+            <p>Yes</p>
+            <p>Yes</p>
+          </div>
+          <hr />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Table;
--- src/pages/Pricing.js
+++ src/pages/Pricing.js
@@ -1,5 +1,10 @@
 import React from "react";
+import Table from "../components/Table";
 
 export default function Pricing() {
-  return <div></div>;
+  return (
+    <div>
+      <Table />
+    </div>
+  );
 }
