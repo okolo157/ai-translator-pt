91d9a4c516930cdb2a98c531ad2307073b03284b
dazycokar <dayocoker15@gmail.com>
2025-02-07 10:07:47 -0800

    latest

--- src/pages/Pricing.js
+++ src/pages/Pricing.js
@@ -1,18 +1,11 @@
 import React from "react";
-<<<<<<< HEAD
 import Table from "../components/Table";
 
-export default function Pricing() {
-  return (
-    <div>
-      <Table />
-=======
 import HelpSection from "../components/HelpSection";
 export default function Pricing() {
   return (
     <div>
-      <HelpSection />
->>>>>>> ce3fbd3baecc746dcb03957edce29491ceb33ede
+      <Table />
     </div>
   );
 }
