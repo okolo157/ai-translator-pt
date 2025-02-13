3c564a67e0a38523b3d50ed1f3c26c1942df82f7
dazycokar <dayocoker15@gmail.com>
2025-02-13 11:51:02 -0800

    change sign up

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -12,10 +12,7 @@ function Signup() {
         <div className="cookies">
           <img src={image} alt="logo" />
           <p> Don't have an account? Signup for free. </p>
-          <button className="btn">
-            <FontAwesomeIcon icon={faGoogle} size="2x" className="itemxx" />
-            sign up with google
-          </button>
+
           <div
             style={{
               display: "flex",
@@ -23,29 +20,67 @@ function Signup() {
               alignItems: "center",
               justifyContent: "center",
             }}
-          >
-            <hr style={{ width: "180px" }} />
-            <p> or </p>
-            <hr style={{ width: "180px" }} />
-          </div>
+          ></div>
         </div>
 
         <div className="cake">
-          <button className="baby">
-            {<FontAwesomeIcon icon={faEnvelope} size="2x" className="itemx" />}{" "}
-            Sign up with Email{" "}
-          </button>
+          <div className="coolinputs">
+            <label for="input" class="text">
+              Username
+            </label>
+            <input
+              type="text"
+              placeholder="Username"
+              className="baby"
+              class="input"
+            />
+            <i></i>
+          </div>
 
+          <div className="coolinputs">
+            <label for="input" class="text">
+              Email
+            </label>
+            <input
+              type="email"
+              placeholder="Sign up with email"
+              className="baby"
+              class="input"
+            />
+            <i></i>
+          </div>
+          <div className="coolinputs">
+            <label for="input" class="text">
+              Password
+            </label>
+            <input
+              type="password"
+              placeholder="Password"
+              className="baby"
+              class="input"
+              name="input"
+            />
+            <i></i>
+          </div>
+          <button class="buttons">Sign up</button>
+
+          {/* 
           <button className="baby">
             {" "}
             <FontAwesomeIcon icon={faApple} size="2x" className="itemx" />
             Sign up with apple
-          </button>
-          <p>
-            {" "}
-            By signing up,you agree to the Terms and conditions and privacy
-            policy.
-          </p>
+          </button> */}
+          <div className="clickboxs">
+            <label>
+              <input type="checkbox" class="input" />
+              <span class="custom-checkbox"></span>
+            </label>
+            <p>
+              {" "}
+              By signing up,you agree to the Terms and conditions and privacy
+              policy.
+            </p>
+          </div>
         </div>
       </div>
     </div>
